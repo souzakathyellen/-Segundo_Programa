ddd35e035f23c81ad47d9d3ad7d65d14c0fa4d76
Kathyellen Souza <87395970+souzakathyellen@users.noreply.github.com>
2022-10-25 16:34:02 -0300

    Add files via upload

---
+++ SegundoProgrma/Segundoprogrma/src/SegundoPrograma.java
@@ -0,0 +1,11 @@
+public class SegundoPrograma {
+    public static void main(String[] args) throws Exception {
+    
+        float nota = 8.5f;
+        System.out.print(" A nota é " + nota);
+
+       
+    }
+
+   
+}
